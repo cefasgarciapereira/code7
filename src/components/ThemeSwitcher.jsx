c5982db374ec54e8253fd03e038b2a935275e77e
Cefas Garcia Pereira <cefasgarciapereira@gmail.com.br>
2021-08-23 19:25:24 -0300

    [feat]: implements ThemeSwitcher

---
+++ src/components/ThemeSwitcher.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { BiAdjust } from "react-icons/bi";
+
+function ThemeSwitcher() {
+    const [mode, setMode] = useState('light')
+
+    useEffect(() => {
+        const storedMode = localStorage.getItem('mode')
+        console.log(storedMode)
+        if (storedMode === 'dark') {
+            setMode('dark')
+        } else {
+            setMode('light')
+        }
+    }, [])
+
+    useEffect(() => {
+        document.querySelector('html')?.setAttribute('data-theme', mode);
+    }, [mode]);
+
+    function toggleTheme() {
+        const newMode = () => mode === 'dark' ? 'light' : 'dark'
+        setMode(newMode())
+        localStorage.setItem('mode', newMode())
+    }
+
+    return (
+        <button
+            className="contrast switcher" 
+            aria-label="Toggle dark mode"
+            onClick={toggleTheme}
+        >
+            <BiAdjust size="1.5em"/>
+        </button>
+    )
+}
+
+export default ThemeSwitcher
