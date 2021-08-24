64c3a888dd6b92d1051a00011786f5f4f30b17cf
Cefas Garcia Pereira <cefasgarciapereira@gmail.com.br>
2021-08-23 22:41:05 -0300

    [feat]: implements card skeleton to loading pages

---
+++ src/elements/CardSkeleton.jsx
@@ -0,0 +1,14 @@
+function CardSkeleton() {
+    return (
+        <article>
+            <header className="skeleton-field">
+            </header>
+            <body>
+
+            </body>
+            <footer className="skeleton-field"></footer>
+        </article>
+    )
+}
+
+export default CardSkeleton
