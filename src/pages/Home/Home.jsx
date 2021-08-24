915b6eed3335d4239f0c6d4d1f4357ce04463a3b
Cefas Garcia Pereira <cefasgarciapereira@gmail.com.br>
2021-08-23 22:41:58 -0300

    [feat]: implements home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,40 @@
+import { useState, useEffect } from 'react'
+import { ClientCard, SearchBar } from 'components'
+import { useFetch } from 'hooks'
+import HomeLoading from './HomeLoading'
+
 function Home() {
+    const { data, loading } = useFetch('https://jsonplaceholder.typicode.com/users')
+    const [clients, setClients] = useState([])
+
+    useEffect(() => {
+        setClients(data)
+    }, [data])
+
+    if (loading) return <HomeLoading />
+
     return (
-        <h1>Home</h1>
+        <div className="container">
+            <header>
+                <SearchBar
+                    placeholder="Busque um cliente pelo nome"
+                    style={{ marginTop: 40 }}
+                    data={data}
+                    searchKey="name"
+                    onSearch={(res) => setClients(res)}
+                />
+            </header>
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-2 g-lg-3">
+                {
+                    clients &&
+                    clients.map((client) =>
+                        <div className="col">
+                            <ClientCard className="p-3" client={client} />
+                        </div>
+                    )
+                }
+            </div>
+        </div>
     )
 }
 
