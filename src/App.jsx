c08558fe957f7dfbdbbcb2e8a49646e262dbd96a
Cefas Garcia Pereira <cefasgarciapereira@gmail.com.br>
2021-08-23 22:39:52 -0300

    [style]: installs grid system from pico-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import '@picocss/pico'
 import 'styles/global.css'
 import 'styles/custom.css'
+import 'styles/grid-system.css'
 
 import { BrowserRouter } from "react-router-dom"
 
 import Routes from './Routes'
-import { ThemeSwitcher } from 'components'
+import { ThemeSwitcher } from 'elements'
 
 function App() {
     return (
