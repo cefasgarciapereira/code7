b9d8229e01147362194c632519a0dde951eacec3
Cefas Garcia Pereira <cefasgarciapereira@gmail.com.br>
2021-08-25 01:08:03 -0300

    [feat]: set up react toastify on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import 'react-toastify/dist/ReactToastify.min.css';
 import '@picocss/pico'
 import 'styles/global.css'
 import 'styles/custom.css'
 import 'styles/grid-system.css'
+import 'styles/animations.css'
+
 
 import { BrowserRouter } from "react-router-dom"
+import { ToastContainer } from 'react-toastify'
 
 import Routes from './Routes'
 import { ThemeSwitcher } from 'elements'
@@ -11,6 +15,20 @@ import { ThemeSwitcher } from 'elements'
 function App() {
     return (
         <BrowserRouter>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                style={{
+                    fontSize: '.75rem'
+                }}
+            />
             <Routes />
             <ThemeSwitcher />
         </BrowserRouter>
