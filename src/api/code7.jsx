fb1b1c61629049f7535d31eaff08cf9a1a161374
Cefas Garcia Pereira <cefasgarciapereira@gmail.com.br>
2021-08-26 15:21:54 -0300

    [feat]: exports API settings

--- src/api/code7.jsx
+++ src/api/code7.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-export default axios.create({
-    baseURL: 'https://provadev.xlab.digital/api/v1/'
-});
-
+export const baseUrl = 'https://provadev.xlab.digital/api/v1/'
 export const uuid = '?uuid=a99fc2e4-3cba-4588-abd3-e4069655deb6'
+
+export default axios.create({
+    baseURL: baseUrl
+});
